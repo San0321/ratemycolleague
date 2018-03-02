496047afa38aad88cc85e7b17db497a2e42d794c
San Kang <san0321@gmail.com>
2018-03-01 19:07:58 -0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,21 @@
+
+    var userdata = [
+        {"username": "dorat", "name": "Rick", "email": "rick@ucsd.edu", "pic":"profile.png", "rating":"Rating: 9/10", "skills": ["React.js", "CSS"], "description" : "Hi, my name is Rick. I have experience on web front-end development. I have used React.js and CSS to develop some websites."},
+        {"username": "something", "name": "Morty", "email": "morty@ucsd.edu", "rating":"Rating: 4/10", "skills": ["PHP", "Node.js"], "description" : "Hi, my name is Morty. I have experience on web back-end development. I have used Node.js and PHP to develop some websites."},
+        {"username": "", "name": "Rick and Morty", "email": "rickandmorty@ucsd.edu", "pic":"RM_profile.png", "skills": ["PHP", "Node.js"], "description" : "Hi, my name is Morty. I have experience on web back-end development. I have used Node.js and PHP to develop some websites."},
+        {"username": "", "name": "Soohorang"},
+        {"username": "", "name": "Bandabi"}
+    ];
+    var message = [
+        {"sender":"", "receiver":"", "message-content":[{
+            "s":"",
+            "r":""
+        }]}
+    ];
+    var users = [
+			{'username':"dorat", 'password':'1234'},
+			{'username':"something", "password": "3456"}
+		];
+    var current = {"username":""}
+
+    var dummyData = [{name: "Phillip Jo"}, {name: "SanKang"}, {name: "yolo"}];
